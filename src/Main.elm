49f0219c13b6c625bc77604dfa3d17c708734495
summerjacket <jasonliang512@gmail.com>
2019-06-26 23:46:57 -0700

    keydown subscription added

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Browser.Events as Events
+import Html exposing (..)
 import Http
 import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Pipeline exposing (optional, required)
@@ -13,21 +13,26 @@ apiHost =
     "https://api.noopschallenge.com"
 
 
-type MazeNode
-    = Nil
-    | MazeNode
-        { north : MazeNode
-        , east : MazeNode
-        , south : MazeNode
-        , west : MazeNode
-        }
-
-
 type CardinalPoint
     = North
     | East
     | South
     | West
+    | Northwest
+    | Northeast
+    | Southwest
+    | Southeast
+
+
+type MazeNode
+    = Wall
+    | Room
+        { locationPath : String
+        , north : MazeNode
+        , east : MazeNode
+        , south : MazeNode
+        , west : MazeNode
+        }
 
 
 
@@ -55,10 +60,10 @@ type alias Pathbot =
 decodePathbot : Decoder Pathbot
 decodePathbot =
     Decode.succeed Pathbot
-        |> required "status" Decode.string
+        |> optional "status" Decode.string ""
         |> optional "message" Decode.string ""
         |> optional "exits" (Decode.list Decode.string) []
-        |> required "description" Decode.string
+        |> optional "description" Decode.string ""
         |> optional "mazeExitDirection" Decode.string ""
         |> optional "mazeExitDistance" Decode.int -1
         |> optional "locationPath" Decode.string ""
@@ -69,18 +74,13 @@ decodePathbot =
 
 
 type alias Model =
-    Pathbot
+    { root : MazeNode
+    }
 
 
 initialModel : Model
 initialModel =
-    { status = "init"
-    , message = "hello world"
-    , exits = []
-    , description = "don't abandon this project like all the others"
-    , mazeExitDirection = "N"
-    , mazeExitDistance = 0
-    , locationPath = ""
+    { root = Wall
     }
 
 
@@ -101,6 +101,7 @@ init =
 
 type Msg
     = GotPathbot (Result Http.Error Pathbot)
+    | MovePlayer (Maybe CardinalPoint)
     | NoOp
 
 
@@ -108,15 +109,61 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotPathbot result ->
+            let
+                _ =
+                    Debug.log "response" result
+            in
             case result of
                 Ok pathbot ->
-                    ( pathbot, Cmd.none )
+                    ( model, Cmd.none )
 
                 Err _ ->
                     ( model, Cmd.none )
 
+        MovePlayer probably ->
+            case probably of
+                Just direction ->
+                    ( model, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
         NoOp ->
             ( model, Cmd.none )
+
+
+
+---- SUBSCRIPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Events.onKeyDown <| Decode.map MovePlayer decodeKey
+
+
+decodeKey : Decoder (Maybe CardinalPoint)
+decodeKey =
+    Decode.field "code" Decode.string
+        |> Decode.map toCardinalPoint
+
+
+toCardinalPoint : String -> Maybe CardinalPoint
+toCardinalPoint str =
+    case str of
+        "ArrowUp" ->
+            Just North
+
+        "ArrowLeft" ->
+            Just West
+
+        "ArrowDown" ->
+            Just South
+
+        "ArrowRight" ->
+            Just East
+
+        _ ->
+            Nothing
 
 
 
@@ -140,5 +187,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
